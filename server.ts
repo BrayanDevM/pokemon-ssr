1a683191eb1820cf1fb6199b87df034889946975
Brayan Ðev <bdevia95@gmail.com>
2025-02-25 17:38:09 -0500

    Angular version updated

---
+++ server.ts
@@ -0,0 +1,13 @@
+import { CommonEngine } from '@angular/ssr/node';
+import { render } from '@netlify/angular-runtime/common-engine';
+
+const commonEngine = new CommonEngine();
+
+export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
+	const pathname = new URL(request.url).pathname;
+	if (pathname === '/api/hello') {
+		return Response.json({ message: 'Hello from the API' });
+	}
+
+	return await render(commonEngine);
+}
